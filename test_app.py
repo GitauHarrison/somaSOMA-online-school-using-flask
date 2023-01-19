e01cc80a1d1aad18fab797bcce7cd2c2b2e49d21
harry <harrisongitau3@gmail.com>
2023-01-19 15:30:58 +0300

    Test: Test access to the landing page

--- test_app.py
+++ test_app.py
@@ -17,16 +17,25 @@ class TestElearningApp(unittest.TestCase):
         self.appctx = self.app.app_context()
         self.appctx.push()
         db.create_all() # < --- create database during setup
+        self.client = self.app.test_client() # < --- test client
 
     def tearDown(self):
         db.drop_all() # < --- discard database after each test
         self.appctx.pop()
         self.app = None
         self.appctx = None
+        self.client = None
 
     def test_elearning_app(self):
         assert self.app is not None
         assert app == self.app
+
+    def test_home_page_access(self):
+        ''''''
+        response = self.client.get('/')
+        response_home = self.client.get('/home')
+        assert response.status_code == 200
+        assert response_home.status_code == 200
 
 
 # Redirection
