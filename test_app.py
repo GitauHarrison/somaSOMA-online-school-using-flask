6c026c8f62bbebd94716a9c694d66175c82145d1
harry <harrisongitau3@gmail.com>
2023-01-19 15:24:16 +0300

    Test: Add a test database

--- test_app.py
+++ test_app.py
@@ -1,24 +1,25 @@
-# Application contect
+# Database configuration is imported first prior to
+# all other configurations
 
-# Redirection
+import os
+os.environ['DATABASE_URL'] = 'sqlite://'
 
-# Access to templates
-
-# Authentication
-
-# Database
-
-
+from app import app, db
 import unittest
-from app import app
+
 
 class TestElearningApp(unittest.TestCase):
+
+    # Application context
+
     def setUp(self):
         self.app = app
         self.appctx = self.app.app_context()
         self.appctx.push()
+        db.create_all() # < --- create database during setup
 
     def tearDown(self):
+        db.drop_all() # < --- discard database after each test
         self.appctx.pop()
         self.app = None
         self.appctx = None
@@ -26,3 +27,10 @@ class TestElearningApp(unittest.TestCase):
     def test_elearning_app(self):
         assert self.app is not None
         assert app == self.app
+
+
+# Redirection
+
+# Access to templates
+
+# Authentication
