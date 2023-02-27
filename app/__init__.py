a364974a9fd7830563078acf90e761d8758d584a
harry <harrisongitau3@gmail.com>
2023-02-27 18:33:21 +0300

    Style: Initialize flask bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 
 
@@ -16,5 +17,6 @@ moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+boostrap = Bootstrap(app)
 
 from app import routes, models, errors
