37a222bc7f4059e211195d054b90c9fe1cf29d95
harry <harrisongitau3@gmail.com>
2023-02-27 19:52:40 +0300

    Build: Add variables to shell context

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from app import app
+from app import app, db
+from app.models import User, Parent, Student, Teacher, Admin
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(
+        db=db,
+        Admin=Admin,
+        Teacher=Teacher,
+        Parent=Parent,
+        Student=Student
+        )
