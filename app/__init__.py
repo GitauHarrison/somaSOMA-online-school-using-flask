c2bd0c71b320ceddac34df3d909f9fba8c209c8f
harry <harrisongitau3@gmail.com>
2023-03-27 22:56:13 +0300

    Build: Send error logs via email

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_moment import Moment
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from config import Config
 
 
@@ -18,5 +21,40 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 boostrap = Bootstrap(app)
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'],
+                    app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='noreply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='somaSOMA Online School Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler(
+            'logs/somaSOMA.log',
+            maxBytes=10240,
+            backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('somaSOMA Online School Startup')
 
 from app import routes, models, errors
